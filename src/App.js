6ea2f8d60d63d4dc2ec688e0228157d026239269
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-11-15 12:46:36 +0200

    added styling, circles and color

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 
-import './App.css';
-
 import React, { Component } from 'react';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Circle from './components/Circle';
+import {circles} from "./components/circles"
+
 
 class App extends Component {
+  state = {
+    circles:circles,
+  }
+
   render() {
     return (
       <div className="App">
+        <Header/>
         <div>
           <h1>Game Speed</h1>
-          <p>Your Score</p>
-          <button>Start Game</button>
-          <button>Stop Game</button>
+          <div className="circles">
+            {
+              this.state.circles.map(circle=>(<Circle 
+                key={circle.id} 
+                color={circle.color} 
+                id={circle.id} />
+              ))
+            }
+          </div>
+          
+          <p>Your Score:</p>
+          <div>
+            <button>Start Game</button>
+            <button>Stop Game</button>
+          </div>
         </div>
+        <Footer/>
     </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+            <p>Femi Adesola &copy; 2021</p>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
