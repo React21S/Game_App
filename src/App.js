60ba12101f9acd1a5a9baf9c022b1e9d2bd3c7ea
FemiAdesola <75606768+FemiAdesola@users.noreply.github.com>
2021-11-15 10:51:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>Game Speed</h1>
+          <p>Your Score</p>
+          <button>Start Game</button>
+          <button>Stop Game</button>
+        </div>
+    </div>
+    );
+  }
+}
+
+export default App;
+
+
